926e0f96d51f7b51003a1a92c906475d44073e6a
justorez <alonezero@foxmail.com>
2020-12-17 22:26:36 +0800

    feat: 进程守护与管理

--- performance/cluster/app.js
+++ performance/cluster/app.js
@@ -2,15 +2,18 @@ const path = require('path');
 const fs = require('fs');
 const http = require('http');
 
+const oom = [];
 http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html;charset=utf-8');
     res.writeHead(200);
 
-    // 这里不对代码进行优化，用于比对使用 cluster 的提升效果
+    // 这里不对代码进行优化，用于直观感受 cluster 的性能提升
     const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
-    // res.write();
+    // oom.push(html); // 模拟内存泄露
     res.end(html);
 })
 .listen(3000, () => {
   console.log('Listening on http://localhost:3000');
+
+  // while(true) {} // 模拟进程卡死
 })
--- performance/cluster/index.js
+++ performance/cluster/index.js
@@ -3,9 +3,74 @@ const os = require('os');
 
 if (cluster.isMaster) {
   for (let i = 0; i < os.cpus().length/2; i++) {
-    cluster.fork();
+    const worker = cluster.fork();
+    checkHeartbeat(worker);
   }
+
+  // 监听子进程退出，重起一个子进程
+  cluster.on('exit', () => {
+    setTimeout(() => {
+      const worker = cluster.fork();
+      checkHeartbeat(worker);
+    }, 3000)
+  })
 } else {
   require('./app');
+
+  process.on('message', (msg) => {
+    if (msg === 'ping') {
+      console.log(`[pong] #${process.pid}`);
+      process.send('pong');
+    }
+  })
+
+  process.on('uncaughtException', (err) => {
+    console.error(err); // 捕捉无法处理的异常，打印或上报错误信息
+
+    process.exit(1);
+  })
+
+  // 监听内存泄露
+  const timer = setInterval(() => {
+    const usage = Math.floor(process.memoryUsage().rss / 1024 / 1024);
+
+    console.log(`[内存使用量] #${process.pid} ${usage}MB`);
+    if (usage > 200) {
+      console.log(`[内存泄露] #${process.pid} OOM!`);
+      clearInterval(timer);
+      process.exit(1);
+    }
+  }, 5000)
+}
+
+/**
+ * 子进程心跳检测
+ * @param {Worker} worker
+ */
+function checkHeartbeat(worker) {
+  let missedPing = 0;
+  const timer = setInterval(() => {
+    if (worker.isDead()) {
+      return clearInterval(timer);
+    }
+
+    console.log(`[ping] #${worker.process.pid}`);
+    worker.send('ping');
+    missedPing++;
+
+    if (missedPing >= 3) {
+      try {
+        worker.exit(1);
+      } catch (error) { // 子进程卡死无法退出，则强制结束
+        process.kill(worker.process.pid);
+      }
+    }
+  }, 3000);
+
+  worker.on('message', (msg) => {
+    if (msg === 'pong') {
+      missedPing--;
+    }
+  });
 }
 
